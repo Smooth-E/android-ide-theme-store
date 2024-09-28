fb438e95713de1ae7f5a8825d0dbb79091e60f14
Smooth-E <smoothie@disroot.org>
2024-09-28 15:50:51 +0300

    fix: Properly set 'selected' for bottom navigation bar entries

--- app/src/main/java/moe/smoothie/androidide/themestore/MainActivity.kt
+++ app/src/main/java/moe/smoothie/androidide/themestore/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,20 +20,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.serialization.Serializable
-import moe.smoothie.androidide.themestore.data.JetbrainsStorefrontResponse
 import moe.smoothie.androidide.themestore.ui.JetbrainsStoreScroller
 import moe.smoothie.androidide.themestore.ui.theme.AndroidIDEThemesTheme
 import okhttp3.OkHttpClient
@@ -53,40 +52,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class TopLevelRoute<T : Any>(
-    val nameResource: Int,
-    val icon: Painter,
-    val route: T
+data class TopLevelRoute(
+    @StringRes val nameResource: Int,
+    @DrawableRes val iconResource: Int,
+    val route: String
 )
-
-@Serializable
-data object JetbrainsMarketplace
-
-@Serializable
-data object VSCodeMarketplace
-
-@Serializable
-data object Settings
 
 @Composable
 fun MainActivityView() {
-    val routes = listOf(
-        TopLevelRoute(
-            R.string.source_jetbrains,
-            painterResource(R.drawable.icons8_jetbrains),
-            JetbrainsMarketplace
-        ),
-        TopLevelRoute(
-            R.string.source_vscode,
-            painterResource(R.drawable.icons8_visual_studio),
-            VSCodeMarketplace
-        ),
-        TopLevelRoute(
-            R.string.destination_settings,
-            painterResource(R.drawable.baseline_settings_24),
-            Settings
-        )
+    val routeJetbrainsMarketplace = TopLevelRoute(
+        R.string.source_jetbrains,
+        R.drawable.icons8_jetbrains,
+        "jetbrains-marketplace"
     )
+    val routeVSCodeMarketplace = TopLevelRoute(
+        R.string.source_vscode,
+        R.drawable.icons8_visual_studio,
+        "vscode-marketplace"
+    )
+    val routeSettings = TopLevelRoute(
+        R.string.destination_settings,
+        R.drawable.baseline_settings_24,
+        "settings"
+    )
+    val routes = listOf(routeJetbrainsMarketplace, routeVSCodeMarketplace, routeSettings)
 
     val navController = rememberNavController()
 
@@ -95,46 +84,17 @@ fun MainActivityView() {
         content = { innerPadding ->
             NavHost(
                 navController = navController,
-                startDestination = JetbrainsMarketplace,
+                startDestination = routeJetbrainsMarketplace.route,
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
             ) {
-                composable<JetbrainsMarketplace> { JetbrainsStoreScroller(it) }
-                composable<VSCodeMarketplace> { PageContent("VSCode themes will be here") }
-                composable<Settings> { PageContent("Settings will be here") }
+                composable(routeJetbrainsMarketplace.route) { JetbrainsStoreScroller(it) }
+                composable(routeVSCodeMarketplace.route) { PageContent("VSCode themes will be here") }
+                composable(routeSettings.route) { PageContent("Settings will be here") }
             }
         },
-        bottomBar = {
-            NavigationBar {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentDestination = navBackStackEntry?.destination
-
-                routes.forEach { route ->
-                    NavigationBarItem(
-                        icon = {
-                            Icon(
-                                painter = route.icon,
-                                contentDescription = stringResource(route.nameResource),
-                                modifier = Modifier.size(24.dp)
-                            )
-                        },
-                        label = { Text(stringResource(route.nameResource)) },
-                        // TODO: Fix route selection
-                        selected = currentDestination != null && currentDestination.route!!::class == route.route::class,
-                        onClick = {
-                            navController.navigate(route.route) {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = true
-                                }
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        }
-                    )
-                }
-            }
-        }
+        bottomBar = { BottomNavigationBar(navController, routes) }
     )
 }
 
@@ -154,5 +114,37 @@ fun PageContent(text: String) {
         verticalArrangement = Arrangement.Center
     ) {
         Text(text)
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavController, routes: List<TopLevelRoute>) {
+    NavigationBar {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+
+        routes.forEach { route ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(route.iconResource),
+                        contentDescription = stringResource(route.nameResource),
+                        modifier = Modifier.size(24.dp)
+                    )
+                },
+                label = { Text(stringResource(route.nameResource)) },
+                // TODO: Fix route selection
+                selected = currentDestination?.route == route.route,
+                onClick = {
+                    navController.navigate(route.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
     }
 }
