2a2c8db9b4ef0e841c3079d35a4e4e8ba19a2552
Smooth-E <smoothie@disroot.org>
2024-10-13 00:02:50 +0300

    ui: New mock up for the theme activity top bar

--- app/src/main/java/moe/smoothie/androidide/themestore/ui/ThemeAppBar.kt
+++ app/src/main/java/moe/smoothie/androidide/themestore/ui/ThemeAppBar.kt
@@ -1,14 +1,18 @@
 package moe.smoothie.androidide.themestore.ui
 
 import android.animation.ArgbEvaluator
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,32 +21,39 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.lerp
+import coil.compose.AsyncImage
 import coil.compose.SubcomposeAsyncImage
 import coil.decode.SvgDecoder
 import coil.request.ImageRequest
@@ -67,31 +78,17 @@ data class ThemeAppBarState(
 @OptIn(ExperimentalMaterial3Api::class)
 fun ThemeAppBar(
     state: ThemeAppBarState,
-    scrollOffset: Int
+    scrollOffset: Int,
+    bakButtonCallback: () -> Unit = { },
+    downloadButtonCallback: () -> Unit = { },
+    openStoreButtonCallback: () -> Unit = { }
 ) {
-    var installButtonWidth by remember { mutableIntStateOf(0) }
+    var contentHeight by remember { mutableIntStateOf(0) }
+    val expandedHeightPx by remember { mutableIntStateOf(0) }
 
-    val headerTextStyle = MaterialTheme.typography.titleLarge
     val downloadsTextStyle = MaterialTheme.typography.bodyMedium
-    val descriptionTextStyle = MaterialTheme.typography.bodyMedium
-    val developerTextStyle = MaterialTheme.typography.bodyMedium
-    val spacing = 8.dp
 
-    val expandedHeightPx =
-        headerTextStyle.lineHeight.toPx() +
-                downloadsTextStyle.lineHeight.toPx() +
-                descriptionTextStyle.lineHeight.toPx() +
-                developerTextStyle.lineHeight.toPx() +
-                spacing.toPx() * 5
-
-    val expandedHeightDp = expandedHeightPx.toDp()
     val collapsedHeightPx = TopAppBarDefaults.MediumAppBarCollapsedHeight.toPx()
-    val collapsedHeightDp = collapsedHeightPx.toDp()
-
-    val actualHeightPx = (expandedHeightPx - scrollOffset)
-        .coerceIn(collapsedHeightPx, expandedHeightPx)
-
-    val actualHeightDp = actualHeightPx.toDp()
 
     val progress = (actualHeightPx - collapsedHeightPx) / (expandedHeightPx - collapsedHeightPx)
 
@@ -108,128 +105,109 @@ fun ThemeAppBar(
             .background(color = backgroundColor)
             .clip(RectangleShape)
     ) {
-        Box(
-            modifier = Modifier
-                .statusBarsPadding()
-                .fillMaxWidth()
-                .height(actualHeightDp),
-        ) {
+        Column(modifier = Modifier.statusBarsPadding()) {
             Row(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .offset(0.dp, (expandedHeightDp - actualHeightDp) / 2)
-                    .requiredHeight(expandedHeightDp),
-                horizontalArrangement = Arrangement.spacedBy(spacing)
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(24.dp)
             ) {
-                val iconSize = lerp(collapsedHeightDp, expandedHeightDp, progress) - spacing * 2
-
-                SubcomposeAsyncImage(
-                    modifier = Modifier
-                        .size(iconSize)
-                        .offset(0.dp, (actualHeightDp - iconSize) / 2)
-                        .clip(CircleShape),
-                    contentDescription = null,
-                    model = if (state.iconUrl.lowercase().endsWith(".svg"))
-                        ImageRequest.Builder(LocalContext.current)
-                            .data(state.iconUrl)
-                            .decoderFactory(SvgDecoder.Factory())
-                            .build()
-                    else
-                        state.iconUrl,
-                    contentScale = ContentScale.Fit
-                )
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(spacing)
-                ) {
-                    val headerVerticalOffset =
-                        ((1f - progress) / 2f * (collapsedHeightPx - headerTextStyle.lineHeight.toPx()))
-                    FadingEdgeMarqueeText(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(
-                                lerp(
-                                    collapsedHeightDp,
-                                    headerTextStyle.lineHeight.toDp(),
-                                    progress
-                                )
-                            )
-                            .padding(
-                                PaddingValues(
-                                    end = installButtonWidth.toDp() * (1f - progress) + spacing
-                                )
-                            )
-                            .offset(0.dp, headerVerticalOffset.toDp()),
-                        text = state.themeName,
-                        style = MaterialTheme.typography.titleLarge,
-                        textAlign = TextAlign.Start,
-                        maxLines = 1,
-                        gradientColors = listOf(
-                            backgroundColor,
-                            Color.Transparent,
-                            Color.Transparent,
-                            Color.Transparent,
-                            Color.Transparent,
-                            backgroundColor
-                        )
+                IconButton(onClick = { bakButtonCallback() }) {
+                    Icon(
+                        painter = painterResource(R.drawable.baseline_arrow_back_24),
+                        contentDescription = stringResource(R.string.content_description_go_back)
                     )
+                }
+                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                    Image(
+                        modifier = Modifier.size(24.dp),
+                        painter = painterResource(R.drawable.jetbrains_marketplace_icon),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = "JetBrains Marketplace", // TODO: Pass store name into the composable
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+                Spacer(Modifier.weight(1f))
+                AnimatedVisibility(
+                    visible = progress == 0f,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    IconButton(onClick = { openStoreButtonCallback() } ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            painter = painterResource(R.drawable.baseline_open_in_new_24),
+                            contentDescription = stringResource(R.string.action_download_theme)
+                        )
+                    }
+                }
+            } // Top part
 
-                    val downloadCounterHeight = downloadsTextStyle.lineHeight.toDp()
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(actualHeightDp)
+            ) {
+                val iconSizeExpanded = 64.dp
+                val iconSizeCollapsed = 24.dp
 
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(downloadCounterHeight)
-                            .alpha(progress),
-                        horizontalArrangement = Arrangement.Absolute.SpaceBetween
+                // Expanded content
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .graphicsLayer(clip = false)
+                        .onGloballyPositioned { contentHeight = it.size.height }
+                ) {
+                    Spacer(Modifier.width(16.dp))
+                    AsyncImage(
+                        model = if (state.iconUrl.lowercase().endsWith(".svg"))
+                            ImageRequest.Builder(LocalContext.current)
+                                .data(state.iconUrl)
+                                .decoderFactory(SvgDecoder.Factory())
+                                .build()
+                        else
+                            state.iconUrl,
+                        modifier = Modifier.size(iconSizeExpanded),
+                        contentDescription = null
+                    )
+                    Spacer(Modifier.width(16.dp))
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
-                        RatingRow(
-                            modifier = Modifier.height(downloadCounterHeight),
-                            rating = 3.5f
+                        val downloadCounterTextStyle = MaterialTheme.typography.bodySmall
+                        val downloadCounterHeight = downloadCounterTextStyle.lineHeight.toDp()
+
+                        Text(
+                            text = state.themeName,
+                            style = MaterialTheme.typography.titleLarge
                         )
                         Row(
-                            modifier = Modifier.fillMaxHeight(),
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Icon(
-                                painter = painterResource(R.drawable.baseline_download_24),
-                                contentDescription = null
+                            RatingRow(
+                                modifier = Modifier.height(downloadCounterHeight),
+                                rating = state.rating
                             )
+                            Spacer(Modifier.weight(1f))
+                            Icon(
+                                modifier = Modifier.size(downloadCounterHeight),
+                                painter = painterResource(R.drawable.baseline_download_24),
+                                contentDescription = null // TODO: Set content description as "Downloads"
+                            )
+                            Spacer(Modifier.width(4.dp))
                             Text(
                                 text = formatNumber(state.downloads),
                                 style = downloadsTextStyle
                             )
                         }
-                    }
-                }
-            }
-            Row(
-                modifier = Modifier
-                    .height(collapsedHeightDp)
-                    .fillMaxWidth()
-                    .padding(8.dp),
-                horizontalArrangement = Arrangement.End
-            ) {
-                androidx.compose.animation.AnimatedVisibility(
-                    visible = progress == 0f,
-                    enter = fadeIn(),
-                    exit = fadeOut()
-                ) {
-                    Button(
-                        modifier = Modifier
-                            .onGloballyPositioned {
-                                installButtonWidth = it.size.width
-                            },
-                        onClick = { },
-                    ) {
-                        Row(
-                            horizontalArrangement = Arrangement.spacedBy(8.dp)
-                        ) {
-                            Icon(
-                                modifier = Modifier.size(18.dp),
-                                painter = painterResource(R.drawable.phosphor_download_simple),
-                                contentDescription = null
-                            )
-                            Text("Install")
-                        }
+
+                        Text(
+                            text = state.description,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
                     }
                 }
             }
@@ -239,7 +217,7 @@ fun ThemeAppBar(
 
 private val mockState = ThemeAppBarState(
     iconUrl = "https://example.com",
-    themeName = "Mtaerial Icons Theme, but it has a longer name",
+    themeName = "Material Icons Theme, but it has a longer name",
     rating = 3.5f,
     downloads = 3_456_789,
     author = "John Doe",
